baaa4f6a01d58dd89f6a0afdf4b456f72e819aff
saymon-felipe <linnubr@gmail.com>
2021-11-29 22:38:48 -0300

    Adição de tags meta e versionamento de js e css

--- js/loadCss.js
+++ js/loadCss.js
@@ -3,12 +3,20 @@ function findDate() {
     let month = date.getMonth();
     let day = date.getDate();
     let year = date.getFullYear();
-    return `${year}-${month}-${day}`
+    
+    // Variavel para fazer a troca pontual dos aquivos JS e CSS
+    let version = 1;
+
+    if (day == 1 || day == 4 || day == 7 || day == 10 || day == 13 || day == 16 || day == 19 || day == 22 || day == 25 || day == 28 || day == 31) {
+        return `${year}-${month}-${day}-v${version}`;
+    }
+    
+    return `${year}-${month}-v${version}`;
 }
 
-function fillCssFile() {
+function fillJsFile() {
     let css = `<link rel="stylesheet" href="css/style.css?v=${findDate()}"></link>`
     $("head").append(css);
 }
 
-fillCssFile();
+fillJsFile();
